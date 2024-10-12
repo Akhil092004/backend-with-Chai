7519b05712c95361cf63a8b61138d5a8d9c4a2c7
Akhil092004 <akhiljain092004@gmail.com>
2024-10-13 00:38:57 +0530

    added cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs"
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
+});
+
+const uploadOnCloudinary = async (localFilePath) =>{
+    try {
+        if(!localFilePath) return null
+        const response = await cloudinary.uploader.upload(localFilePath,{
+            resource_type: "auto"
+        })
+
+        //file has been uploaded
+        console.log("file is uploaded on cloudinary", response.url);
+
+        return response
+        
+    } catch (error) {
+        fs.unlinkSync(localFilePath) //remove the locally saved temporary file
+
+        return null
+    }
+}
+
+export {uploadOnCloudinary}
