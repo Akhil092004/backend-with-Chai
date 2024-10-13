c3bf394f2ad619e60ec1d6eb28b8369483c4d81b
Akhil092004 <akhiljain092004@gmail.com>
2024-10-13 19:07:06 +0530

    All Setup Configured

--- src/app.js
+++ src/app.js
@@ -19,4 +19,15 @@ app.use(express.static("public"))
 
 app.use(cookieParser())
 
+
+//routes import
+import userRouter from "./routes/user.routes.js"
+
+
+//routes declaration
+app.use("/api/v1/users",userRouter) 
+// http;//localhost:8000/api/v1/users/<route>
+
+
+
 export {app}
--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,6 @@ import {DB_NAME} from "../constants.js"
 const connectDB = async () => {
     try{
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(connectionInstance)
         console.log(`\n MongoDB connected !! DB_Host : ${connectionInstance.connection.host}`)
     }
     catch {
