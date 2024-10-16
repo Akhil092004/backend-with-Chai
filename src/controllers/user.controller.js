c54d5e2122af0b6498f5ec8422533c9382c0d75b
Akhil092004 <akhiljain092004@gmail.com>
2024-10-17 00:29:27 +0530

    Debugging, Postmen Checking and file unlink

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -28,7 +28,7 @@ const registerUser = asyncHandler(async (req,res) => {
         throw new ApiError(400,"All Fields are compulsary"); 
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or:[{username},{email}]
     })
 
@@ -38,18 +38,23 @@ const registerUser = asyncHandler(async (req,res) => {
 
     //given my multer
     const avatarLocalPath = req.files?.avatar[0]?.path
-    const coverImageLocalPath = req.files?.coverImage[0]?.path
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path
 
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
 
     if(!avatarLocalPath){
-        throw new ApiError(400,"Avatar file is required")
+        throw new ApiError(400,"Avatar file is required :: can't find local Path")
     }
     
     const avatar =  await uploadOnCloudinary(avatarLocalPath)
     const coverImage =  await uploadOnCloudinary(coverImageLocalPath)
 
+
     if(!avatar){
-        throw new ApiError(400,"Avatar file is required")
+        throw new ApiError(400,"Avatar file is required :: couldNot upload to cloudinary")
     }
 
 
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -15,8 +15,10 @@ const uploadOnCloudinary = async (localFilePath) =>{
         })
 
         //file has been uploaded
-        console.log("file is uploaded on cloudinary", response.url);
+        // console.log("file is uploaded on cloudinary", response.url);
+        // console.log(response);
 
+        fs.unlinkSync(localFilePath)
         return response
         
     } catch (error) {
